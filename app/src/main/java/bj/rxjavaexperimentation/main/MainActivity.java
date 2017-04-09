daee3420868f6d935d1d9c19d69be8586971327a
josh_laird <jbmlaird@gmail.com>
2017-04-10 00:43:40 +0100

    Placeholder implementation for navigation drawer

--- app/src/main/java/bj/rxjavaexperimentation/main/MainActivity.java
+++ app/src/main/java/bj/rxjavaexperimentation/main/MainActivity.java
@@ -2,8 +2,17 @@ package bj.rxjavaexperimentation.main;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.holder.BadgeStyle;
+import com.mikepenz.materialdrawer.model.ExpandableDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 
 import javax.inject.Inject;
 
@@ -11,14 +20,17 @@ import bj.rxjavaexperimentation.AppComponent;
 import bj.rxjavaexperimentation.R;
 import bj.rxjavaexperimentation.common.BaseActivity;
 import bj.rxjavaexperimentation.search.SearchActivity;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends BaseActivity implements MainContract.View
 {
     private static final String TAG = "MainActivity";
 
+    @BindView(R.id.toolbar) Toolbar toolbar;
     @Inject MainPresenter presenter;
     private MainComponent mainComponent;
+    private Drawer result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -26,6 +38,10 @@ public class MainActivity extends BaseActivity implements MainContract.View
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        // Separate logic if the user isn't logged in
+        // Force user login?
+        result = presenter.buildNavigationDrawer(this, toolbar);
     }
 
     @Override
@@ -52,5 +68,19 @@ public class MainActivity extends BaseActivity implements MainContract.View
     {
         getMenuInflater().inflate(R.menu.options_menu, menu);
         return true;
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        //handle the back press :D close the drawer first and if the drawer is closed close the activity
+        if (result != null && result.isDrawerOpen())
+        {
+            result.closeDrawer();
+        }
+        else
+        {
+            super.onBackPressed();
+        }
     }
 }
